a2a87c585274531831ed96fceda62b581b72196e
Lucas Gomes <lucasggomees@gmail.com>
2022-05-22 21:55:28 +0100

    Created ComputerPlay function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
 (function () {
     'use strict';
 
+    const computerPlay = () => {
+
+        let rand = Math.floor(Math.random() * 16);
+        console.log(rand);
+        if (rand <= 5) {
+            return ("rock");
+        }
+        else if (rand <= 10) {
+            return ("paper");
+        } else {
+            return ("scissors");
+        }
+    }
+
+    const playRound = (playerSelection, computerSelection) => { }
+
+    console.log(computerPlay());
 })()
