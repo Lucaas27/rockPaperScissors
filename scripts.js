928335c010b9828b6b0e7e801f4d5562830f833b
Lucas Gomes <lucasggomees@gmail.com>
2022-05-22 22:53:09 +0100

    Created playRound function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 (function () {
     'use strict';
 
-    const computerPlay = () => {
+    const promptUser = prompt("Rock, paper or scissors?", "")
+    let playerSelection = promptUser.toLowerCase();
+    let computerSelection = computerPlay();
+
+    function computerPlay() {
 
         let rand = Math.floor(Math.random() * 16);
         console.log(rand);
@@ -15,7 +19,40 @@
         }
     }
 
-    const playRound = (playerSelection, computerSelection) => { }
+    const playRound = (playerSelection, computerSelection) => {
 
-    console.log(computerPlay());
+        if (playerSelection === computerSelection) {
+            console.log(`playerSelection: ${playerSelection}`);
+            console.log(`computerSelection: ${computerSelection}`);
+            return `It's a draw ! You both chose ${playerSelection}.`
+        }
+        else if (
+            playerSelection === "paper" && computerSelection === "rock"
+            ||
+            playerSelection === "rock" && computerSelection === "scissors"
+            ||
+            playerSelection === "scissors" && computerSelection === "paper"
+        ) {
+            console.log(`playerSelection: ${playerSelection}`);
+            console.log(`computerSelection: ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+        }
+        else if (
+            computerSelection === "paper" && playerSelection === "rock"
+            ||
+            computerSelection === "rock" && playerSelection === "scissors"
+            ||
+            computerSelection === "scissors" && playerSelection === "paper"
+        ) {
+            console.log(`playerSelection: ${playerSelection}`);
+            console.log(`computerSelection: ${computerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        }
+        else {
+            console.warn("Incorrect Value !!");
+            return `You Lose! Invalid choice`
+        }
+    }
+
+    console.log(playRound(playerSelection, computerSelection));
 })()
